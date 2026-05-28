b4e581da96dab7041e4aabe31297472ec3d167be
Aniagolu Chiemelie ® <123953064+aniagoluchiemelie77@users.noreply.github.com>
2026-05-28 22:13:47 +0100

    Added inapp constants

---
+++ constants/inAppConstants.js
@@ -0,0 +1,12 @@
+export const USD_SUBSCRIPTION_PRICES = Object.freeze({
+  Pro: 1.11,
+  Premium: 3.69,
+  Free: 0,
+});
+export const EXCEPTION_COST_IN_ICASH = 0.5;
+export const USD_EQUIVALENCE_OF_1_ICASH = 0.74;
+export const EXCEPTION_ACCOUNT_LIMITS = Object.freeze({
+  free: 1,
+  pro: 2,
+  premium: 3,
+});
